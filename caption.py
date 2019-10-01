b3e84fb928139885dc32685be91e60aae52441b4
kanishk <kanishk5991@gmail.com>
2019-10-01 06:49:47 +0530

    save image during caption generation

--- caption.py
+++ caption.py
@@ -182,7 +182,7 @@ def visualize_att(image_path, seq, alphas, rev_word_map, smooth=True):
             plt.imshow(alpha, alpha=0.8)
         plt.set_cmap(cm.Greys_r)
         plt.axis('off')
-    plt.show()
+    plt.savefig(image_path.split("/")[-1][:-4]+'.jpg')
 
 
 if __name__ == '__main__':
